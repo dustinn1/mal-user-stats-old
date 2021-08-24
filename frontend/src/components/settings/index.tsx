25005fdaead1321b0003bd70c482b601ce6167c6
Dustin Nguyen <dustin7212@gmail.com>
2021-08-24 17:00:24 -0500

    Update settings modal

--- frontend/src/components/settings/index.tsx
+++ frontend/src/components/settings/index.tsx
@@ -1,9 +1,13 @@
 import { useContext } from "react";
+import { LanguageContext } from "../../contexts/LanguageContext";
+import { ThemeContext } from "../../contexts/ThemeContext";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import { LanguageContext } from "../../contexts/LanguageContext";
-import { ThemeContext } from "../../contexts/ThemeContext";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon, faDesktop } from "@fortawesome/free-solid-svg-icons";
 import "./styles.css";
 
 interface Props {
@@ -23,54 +27,69 @@ export default function Settings(props: Props) {
       <Modal.Body>
         <Container>
           <span>Theme</span>
-          <div className="settings-modal-buttons">
-            <Button
-              variant="outline-primary"
-              onClick={() => theme.updateTheme("system")}
-              active={theme.theme === "system"}
-            >
-              System
-            </Button>{" "}
-            <Button
-              variant="outline-primary"
-              onClick={() => theme.updateTheme("light")}
-              active={theme.theme === "light"}
-            >
-              Light
-            </Button>{" "}
-            <Button
-              variant="outline-primary"
-              onClick={() => theme.updateTheme("dark")}
-              active={theme.theme === "dark"}
-            >
-              Dark
-            </Button>{" "}
-          </div>
+          <Row className="text-center">
+            <Col>
+              <Button
+                variant="outline-primary"
+                onClick={() => theme.updateTheme("system")}
+                active={theme.theme === "system"}
+              >
+                <FontAwesomeIcon icon={faDesktop} className="me-2" />
+                System
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="outline-primary"
+                onClick={() => theme.updateTheme("light")}
+                active={theme.theme === "light"}
+              >
+                <FontAwesomeIcon icon={faSun} className="me-2" />
+                Light
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="outline-primary"
+                onClick={() => theme.updateTheme("dark")}
+                active={theme.theme === "dark"}
+              >
+                <FontAwesomeIcon icon={faMoon} className="me-2" />
+                Dark
+              </Button>
+            </Col>
+          </Row>
           <br />
           <span>Title Language</span>
-          <div className="settings-modal-buttons">
-            <Button
-              variant="outline-primary"
-              onClick={() => language.updateLanguage("romaji")}
-              active={language.language === "romaji"}
-            >
-              Romaji
-            </Button>{" "}
-            <Button
-              variant="outline-primary"
-              onClick={() => language.updateLanguage("english")}
-              active={language.language === "english"}
-            >
-              English
-            </Button>{" "}
-            <Button
-              variant="outline-primary"
-              onClick={() => language.updateLanguage("japanese")}
-              active={language.language === "japanese"}
-            >
-              日本語
-            </Button>
-          </div>
+          <Row className="text-center">
+            <Col>
+              <Button
+                variant="outline-primary"
+                onClick={() => language.updateLanguage("romaji")}
+                active={language.language === "romaji"}
+              >
+                Romaji
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="outline-primary"
+                onClick={() => language.updateLanguage("english")}
+                active={language.language === "english"}
+              >
+                English
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="outline-primary"
+                onClick={() => language.updateLanguage("japanese")}
+                active={language.language === "japanese"}
+              >
+                日本語
+              </Button>
+            </Col>
+          </Row>
         </Container>
       </Modal.Body>
       <Modal.Footer>
