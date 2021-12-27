38e60190c94b764741582102dd33df5a65d4640d
Dustin Nguyen <dustin7212@gmail.com>
2021-12-26 18:41:50 -0600

    Remove backend, move frontend files up a level

--- backend/src/routes/stats.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getFullStats } from "../controllers/stats/statsController";
-
-const router = express.Router();
-
-router.post("/generate", getFullStats);
-
-export default router;
